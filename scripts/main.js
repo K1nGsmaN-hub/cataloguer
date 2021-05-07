70907398f6f73b31cae4702a65315b4705387b94
K1nGsmaN-hub <ilya@avex.kz>
2021-05-08 04:16:15 +0800

    added global vars in script/main.js

---
+++ scripts/main.js
@@ -0,0 +1,19 @@
+const table = document.querySelector('table'),
+      tableInner = document.querySelector('table tbody').innerHTML
+
+const modalFields = {
+  modalBG: null,
+  btnClose: null,
+  btnSubmit: null,
+  modalID: null,
+  modalTitle: null,
+  modalAuthor: null,
+  modalDescr: null,
+  modalEDU: null,
+  modalCategory: null,
+  modalSection: null,
+  modalWork: null,
+  modalFile: null,
+  modalSpecialty: null,
+  modalTheme: null
+}
