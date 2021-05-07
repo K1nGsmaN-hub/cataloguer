4dc90bc1c7de0c0840739aded3697ee689407262
K1nGsmaN-hub <ilya@avex.kz>
2021-05-08 04:15:21 +0800

    added update resource functional in update-by-id.php

---
+++ update-by-id.php
@@ -0,0 +1,21 @@
+<?php
+  include("connect.php");
+
+  $codedData = trim(file_get_contents("php://input"));
+  $data = json_decode($codedData, true);
+
+  $resource = R::load('resources', $data['id']);
+
+  $resource->title = $data['title'];
+  $resource->author = $data['author'];
+  $resource->descr = $data['descr'];
+  $resource->edu = $data['edu'];
+  $resource->category = $data['category'];
+  $resource->section = $data['section'];
+  $resource->specialty = $data['specialty'];
+  $resource->work_type = $data['work_type'];
+  $resource->file_type = $data['file_type'];
+  $resource->theme = $data['theme'];
+
+  R::store($resource);
+?>
